d2e34c072ba1b781f882320368ae0a5518d14cb8
Leo <leo@cacheux.net>
2025-02-08 12:58:26 +0100

    Set app version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,8 +106,8 @@ android {
         applicationId = "net.cacheux.nvp.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.compileSdk.get().toInt()
-        versionCode = 10101
-        versionName = "1.1.1"
+        versionCode = 10102
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "net.cacheux.nvp.app.CustomTestRunner"
         vectorDrawables {
